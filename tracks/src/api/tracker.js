4d2faa89016ee30012da1518ef01ae119b0fb237
Xihai Luo <xihai.luo01@gmail.com>
2023-03-22 21:44:30 -0400

    handle axios error for signup

---
+++ tracks/src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL:"https://b2fa-70-30-40-247.ngrok.io",
+});
--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -1,17 +1,30 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, error_message: action.payload };
     default:
       return state;
   }
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
+  return async ({ email, password }) => {
     // make api request to sign up with email and password
     // if request succeds, modify our state and say we are authenticated
     // if fails, show some error message
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      dispatch({
+        type: "add_error",
+        payload: "Something went wrong with sign up",
+      });
+      console.log(err.message);
+    }
   };
 };
 
@@ -32,5 +45,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signup, signin, signout },
-  { isSignedIn: false }
+  { isSignedIn: false, error_message: "" }
 );
