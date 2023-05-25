b6b67c7eeaae61f6ed3c18cd221d0c4e1bfb8ad4
Xihai Luo <xihai.luo01@gmail.com>
2023-05-25 17:07:20 -0400

    add code to mock gps location

--- tracks/src/screens/TrackCreateScreen.js
+++ tracks/src/screens/TrackCreateScreen.js
@@ -1,8 +1,13 @@
+import "../_mockLocation";
 import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import { requestForegroundPermissionsAsync } from 'expo-location';
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
@@ -11,8 +16,18 @@ const TrackCreateScreen = () => {
   const startWatching = async () => {
     try {
       const { granted } = await requestForegroundPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
       if (!granted) {
-        throw new Error('Location permission not granted');
+        throw new Error("Location permission not granted");
       }
     } catch (e) {
       setErr(e);
